731ea6eb690390adc6cb8f86c178aab3c2aac17e
TristanStreich <tristanstreich@gmail.com>
2023-06-11 11:05:52 -0700

    m

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.OpenURL("http://localhost:3000/token/gen");
     }
 
     // Update is called once per frame
