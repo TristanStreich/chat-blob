4c5cd196c983d584f0c27994115cb73bf178aafa
Remy Streichenberger <36056452+Bonus456@users.noreply.github.com>
2023-06-14 01:00:11 -0700

    Blob Click and Drag

---
+++ Assets/ClickandDrag.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickandDrag : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector2 previousMousePosition;
+    private Rigidbody2D clickedRigidbody;
+    private Rigidbody2D[] dynamicBodies;
+
+    private void Start()
+    {
+        GameObject[] bodyObjects = GameObject.FindGameObjectsWithTag("body");
+        dynamicBodies = new Rigidbody2D[bodyObjects.Length];
+        for (int i = 0; i < bodyObjects.Length; i++)
+        {
+            dynamicBodies[i] = bodyObjects[i].GetComponent<Rigidbody2D>();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        //Debug.Log("Clicked");
+        isDragging = true;
+        previousMousePosition = GetMouseWorldPosition();
+
+        clickedRigidbody = GetComponent<Rigidbody2D>();
+        clickedRigidbody.bodyType = RigidbodyType2D.Static;
+
+        MakeDynamicBodiesStayDynamic();
+    }
+
+    private void OnMouseUp()
+    {
+       // Debug.Log("letGo");
+        isDragging = false;
+
+        clickedRigidbody.bodyType = RigidbodyType2D.Dynamic;
+        clickedRigidbody = null;
+
+        MakeDynamicBodiesStayDynamic();
+    }
+
+    private void OnMouseDrag()
+    {
+        if (isDragging)
+        {
+            Vector2 currentMousePosition = GetMouseWorldPosition();
+            Vector2 displacement = currentMousePosition - previousMousePosition;
+            transform.position += (Vector3)displacement;
+            previousMousePosition = currentMousePosition;
+        }
+    }
+
+    private void MakeDynamicBodiesStayDynamic()
+    {
+        foreach (Rigidbody2D body in dynamicBodies)
+        {
+            if (body != clickedRigidbody)
+            {
+                body.bodyType = RigidbodyType2D.Dynamic;
+            }
+        }
+    }
+
+    private Vector2 GetMouseWorldPosition()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        return mousePosition;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,16 +3,56 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
+    public string bodyTag = "body";
+    public float searchRadius = 1f;
+
+    private void Update()
     {
-    
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(mousePosition, searchRadius);
+
+            if (colliders.Length > 0)
+            {
+                Collider2D nearestCollider = GetNearestCollider(colliders, mousePosition);
+                if (nearestCollider.CompareTag(bodyTag))
+                {
+                    GrabObject(nearestCollider.gameObject, mousePosition);
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private Collider2D GetNearestCollider(Collider2D[] colliders, Vector2 referencePosition)
     {
-        
+        Collider2D nearestCollider = colliders[0];
+        float nearestDistance = Vector2.Distance(referencePosition, nearestCollider.transform.position);
+
+        for (int i = 1; i < colliders.Length; i++)
+        {
+            float distance = Vector2.Distance(referencePosition, colliders[i].transform.position);
+            if (distance < nearestDistance)
+            {
+                nearestDistance = distance;
+                nearestCollider = colliders[i];
+            }
+        }
+
+        return nearestCollider;
+    }
+
+    private void GrabObject(GameObject obj, Vector2 position)
+    {
+        // Snap the object to the mouse position
+        obj.transform.position = position;
+
+        // Perform any additional logic for grabbing the object
+        ClickandDrag clickAndDrag = obj.GetComponent<ClickandDrag>();
+        if (clickAndDrag != null)
+        {
+            //clickAndDrag.OnMouseDown();
+            //clickAndDrag.OnMouseDrag(); // Trigger the OnMouseDrag event
+        }
     }
 }
